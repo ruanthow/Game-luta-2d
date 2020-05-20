e8db0b2c151722d55ae204f99a78bae76eb0951b
Higor Oliveira <higorosantos@gmail.com>
2020-05-20 19:30:53 -0300

    add character izaoyi

---
+++ Assets/Scripts/movePlayers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayers : MonoBehaviour
+{
+    private Rigidbody2D rg;
+    private float hMove;
+    private float vMove;
+    private float speed = 6;
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+       
+    }
+    private void Move()
+    {
+        hMove = Input.GetAxis("Horizontal");
+        rg.velocity = new Vector2(hMove * speed, rg.velocity.y);
+    }
+    private void Jump()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+         
+            
+        }
+       
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+}
